5d1d0cd6b56f11374f8a1db3024ce71fcaca19da
Allie K <allie@Allisons-MacBook-Pro.local>
2020-03-11 20:24:50 -0500

    Bug Fix

--- static/js/app.js
+++ static/js/app.js
@@ -2,11 +2,13 @@ const input = d3.select("input")
 const button = d3.select("button")
 const table = d3.select("body").append("table")
 
+const allData = data
 
-function buildTable() {
+
+function buildTable(dataToShow) {
     const tbody = d3.select('tbody')
     tbody.html("")
-    data.forEach(dataRow => {
+    dataToShow.forEach(dataRow => {
         const elements = tbody.append('tr')
         Object.values(dataRow).forEach(value => tbody.append('td').text(value))
     })
@@ -15,30 +17,21 @@ function buildTable() {
 
 const handler = function(){
     const tbody = d3.select('tbody')
-    // tbody.html("")
-    data.forEach(dataRow => {
-        const elements = tbody.append('tr')
-        Object.values(dataRow).forEach(value => tbody.append('td').text(value))
-    })
 
   let filterCond = input.property("value")
+  console.log(filterCond)
 
-  tbody.html("")
+if (filterCond === ""){
+    buildTable(allData)
+} else {
 
-  data.filter(record => record.datetime === filterCond).forEach(record => {
-    let row = tbody.append("tr")
-    row.append("td").text(record.datetime)
-    row.append("td").text(record.city)
-    row.append("td").text(record.state)
-    row.append("td").text(record.country)
-    row.append("td").text(record.shape)
-    row.append("td").text(record.durationMinutes)
-    row.append("td").text(record.comments)
+  const filteredData = data.filter(record => record.datetime === filterCond)
+  buildTable(filteredData)
 
-    })
+}
 }
 
-buildTable()
+buildTable(allData)
 input.on("change", handler)
 button.on("click", handler)
 
